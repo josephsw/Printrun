98c6e3d6c4c6e51c86f0b1af50276cfc07e8e0a4
Joseph W <joseph.shen.wang@gmail.com>
2014-01-14 19:35:38 -0800

    Add fie reading functionality and Pronsole function call stubs

--- pronsole.py
+++ pronsole.py
@@ -19,12 +19,85 @@ import sys
 import traceback
 from printrun.pronsole import pronsole
 
+def do_connect(l):
+    return
+
+def do_load(l):
+    return
+
+def do_print(l):
+    return
+
+def do_settemp(l):
+    return
+
+def do_home(l):
+    return
+
+def do_move(l):
+    return
+
+def do_extrude(l):
+    return
+
+def do_monitor(l):
+    return
+
+def do_sleep(l):
+    return
+
+def do_settempwait(l):
+    return
+
+# Convert command.txt strings into function calls
+funcdict = {
+    "connect": do_connect,
+    "load": do_load,
+    "print": do_print,
+    "settemp": do_settemp,
+    "home": do_home,
+    "move": do_move,
+    "extrude": do_extrude,
+    "monitor": do_monitor,
+    "sleep": do_sleep,
+    "settempwait": do_settempwait,
+}
+
 if __name__ == "__main__":
 
     interp = pronsole()
-    interp.parse_cmdline(sys.argv[1:])
+    '''interp.parse_cmdline(sys.argv[1:])
     try:
-        interp.cmdloop()
+        interp.cmdloop()'''
+
+    # SYNTAX:
+    # connect <port> <baud>
+    # load <file>
+    # print
+    # settemp <temp/abs/off/pla>
+    # home <x/y/z>
+    # move <x/y/z> <val>
+    # extrude <amount> <speed>
+    # monitor <secs>
+
+    # CUSTOM SYNTAX:
+    # sleep <secs>
+    # settempwait <temp/abs/off/pla>
+
+
+    try:
+        commandfile = open("commands.txt", "rU")
+
+        # Read commands from file
+        while (line = commandfile.readline().strip().lower()):
+            cmdlist = line.split()
+            if len(line) < 1:
+                break
+
+            line = line[len(cmdlist[0])+1:]
+
+            funcdict[cmdlist[0]](line)
+            
     except SystemExit:
         interp.p.disconnect()
     except:
